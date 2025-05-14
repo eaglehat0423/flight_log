31e062096afd1f87e9ddf9c28af32e319a6ee6aa
服部 恭弥 <s252626@asano.eps.s.u-tokyo.ac.jp>
2025-05-14 15:30:32 +0900

    loglistをデザイン

--- flightlog/templatetags/custom_filters.py
+++ flightlog/templatetags/custom_filters.py
@@ -8,3 +8,11 @@ def add_class(field, css_class):
         return field.as_widget(attrs={"class": css_class})
     return field  # フィールドでない場合はそのまま返す
 
+
+@register.filter(name='extract_iata')
+def extract_iata(value):
+    if value:
+        parts = value.split('/')
+        if parts:
+            return parts[0]
+    return value  # 返す値がない場合、そのまま返す
